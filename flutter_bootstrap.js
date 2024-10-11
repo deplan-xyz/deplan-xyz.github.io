e6115cb97285973976b4468ce3ca84dab18d4519
Anton Pashkovskyi <anton@globalbit.co.il>
2024-10-11 20:43:20 +0200

    build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3475688462"
+    serviceWorkerVersion: "1626163363"
   }
 });
